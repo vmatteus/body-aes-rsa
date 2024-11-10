9ad84310eedcef76094c578b2712622d451d3643
Vinícius Matteus <vmatteus@gmail.com>
2024-11-10 10:30:25 -0300

    add generate .env.test

---
+++ scripts/generateEnvTest.js
@@ -0,0 +1,15 @@
+// scripts/generateEnvTest.js
+const fs = require('fs');
+const path = require('path');
+
+const envTestPath = path.join(__dirname, '../.env.test');
+
+if (!fs.existsSync(envTestPath)) {
+  const envTestContent = `
+  RSA_PRIVATE_KEY=
+  RSA_PUBLIC_KEY=
+  `;
+
+  fs.writeFileSync(envTestPath, envTestContent, { encoding: 'utf-8' });
+  console.log('The .env.test file has been created successfully');
+} 
